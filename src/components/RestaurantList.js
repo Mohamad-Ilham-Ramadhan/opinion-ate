d3ba55148700f8da5def5f08869b6900332ed1b7
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2022-04-28 20:25:08 +0700

    display the restaurants

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,9 +1,18 @@
 import {useEffect} from 'react';
 
-export const RestaurantList = ({loadRestaurants}) => {
+export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
-  return <div>Restaurants</div>;
+  return (
+    <div>
+      <div>Restaurants</div>
+      <ul>
+        {restaurants.map(restaurant => (
+          <li key={restaurant.id}>{restaurant.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default RestaurantList;
