05814064e3c452b7ab9ea48116c84d4572d30f1f
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2022-05-05 13:16:52 +0700

    edge cases: add loading indicator, error flag

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react';
+import {scryRenderedComponentsWithType} from 'react-dom/test-utils';
 import {RestaurantList} from '../RestaurantList'; // Be sure to use the named import import {RestaurantList} with curly braces, not the default import import RestaurantList. The named import will continue to be the component that is not connected to Redux, which is the one we want to unit test. If you use the default import, then once we connect it to Redux your unit test will begin failing.
 describe('RestaurantList', () => {
   const restaurants = [
@@ -7,22 +8,50 @@ describe('RestaurantList', () => {
   ];
   let loadRestaurants;
 
-  beforeEach(() => {
-    loadRestaurants = jest.fn().mockName('loadRestaurants'); // .mockName() to give our mock function a name to make our error messages more readable.
+  const renderWithProps = (propOverrides = {}) => {
+    const props = {
+      loadRestaurants: jest.fn().mockName('loadRestaurants'),
+      loading: false,
+      restaurants,
+      ...propOverrides,
+    };
+    loadRestaurants = props.loadRestaurants;
+    render(<RestaurantList {...props} />);
+  };
 
-    render(
-      <RestaurantList
-        loadRestaurants={loadRestaurants}
-        restaurants={restaurants}
-      />,
-    );
-  });
   it('loads restaurants on first render', () => {
+    renderWithProps();
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
-  it('display the restaurants', () => {
-    expect(screen.queryByText('Sushi Place')).not.toBeNull();
-    expect(screen.queryByText('Pizza Place')).not.toBeNull();
+  it('displays the loading indicator while loading', () => {
+    renderWithProps({loading: true});
+    expect(screen.queryByTestId('loading-indicator')).not.toBeNull();
+  });
+
+  describe('when loading fails', () => {
+    it('displays the error message', () => {
+      renderWithProps({loadError: true});
+      expect(
+        screen.getByText('Restaurants could not be loaded.'),
+      ).toBeInTheDocument();
+    });
+  });
+
+  describe('When loading succeeds', () => {
+    it('does not display the loading indicator while not loading', () => {
+      expect(screen.queryByTestId('loading-indicator')).toBeNull();
+    });
+    it('does not display the error message', () => {
+      renderWithProps();
+      expect(
+        screen.queryByText('Restaurants could not be loaded.'),
+      ).not.toBeInTheDocument();
+    });
+    it('display the restaurants', () => {
+      renderWithProps();
+      expect(screen.queryByText('Sushi Place')).not.toBeNull();
+      expect(screen.queryByText('Pizza Place')).not.toBeNull();
+    });
   });
 });
--- src/store/restaurants/actions.js
+++ src/store/restaurants/actions.js
@@ -1,12 +1,22 @@
 export const STORE_RESTAURANTS = 'STORE_RESTAURANTS';
+export const START_LOADING = 'START_LOADING';
+export const RECORD_LOADING_ERROR = ' RECORD_LOADING_ERROR';
 
-export const loadRestaurants = () => (dispatch, getState, api) => {
-  api.loadRestaurants().then(records => {
+export const loadRestaurants = () => async (dispatch, getState, api) => {
+  try {
+    dispatch(startLoading());
+    const records = await api.loadRestaurants();
     dispatch(storeRestaurants(records));
-  });
+  } catch {
+    dispatch(recordLoadingError());
+  }
 };
 
 const storeRestaurants = records => ({
   type: STORE_RESTAURANTS,
   records,
 });
+
+const startLoading = () => ({type: START_LOADING});
+
+const recordLoadingError = () => ({type: RECORD_LOADING_ERROR});
